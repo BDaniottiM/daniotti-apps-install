055b5a7c01cc1e6cbbc67a122534e17a4057a711
Daniotti <63687482+BDaniottiM@users.noreply.github.com>
2024-10-06 01:30:37 -0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<div class="linha">
+		<a href="https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x64" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/pngwing.com.png" alt="Discord">
+		</a>
+		<a href="https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/epic-games-logo-0.png" alt="Epic Games">
+		</a>
+		<a href="https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/[CITYPNG.COM]HD Steam Square Icon PNG - 2000x2000.png" alt="Steam">
+		</a>
+		<a href="https://us.download.nvidia.com/nvapp/client/10.0.3.152/NVIDIA_app_beta_v10.0.3.152.exe" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/nvidia-logo-vert (1).png" alt="Nvidia App">
+		</a>
+		<a href="https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/logitech.png" alt="GHub">
+		</a>
+		<a href="https://d1z8x2chb1ujcd.cloudfront.net/software-installers/Ngenuity_Installer/HyperX_NGENUITY_Installer_2.25.7.0.exe" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/hyper-x-logo (1).png" alt="NGenuity">
+		</a>
+		<a href="https://flagship-versions.voicemod.net/1.2.1/VoicemodInstaller_1.2.1.exe" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/Voicemod-logo.jpg" alt="Voicemod">
+		</a>
+	</div>
+	
+	<div class="linha">
+		<a href="https://www.leagueoflegends.com/pt-br/download/" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/Lol.png" alt="LOL">
+		</a>
+		<a href="https://runtime.fivem.net/client/FiveM.exe" target="_blank">
+			<img src="https://r2.fivemanage.com/9hrGBNk9Ht1K4lG2ZWXli/images/fivem.png" alt="FiveM">
+		</a>
+	</div>
+</main>
